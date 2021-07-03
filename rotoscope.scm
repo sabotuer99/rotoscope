c4aef1ea49a14a03b52a4485e0a4ab4254c19a63
Troy Whorten <sabotuer99@gmail.com>
2021-07-02 21:19:00 -0700

    rotoscope script producing interesting output

--- rotoscope.scm
+++ rotoscope.scm
@@ -14,6 +14,140 @@
     )
 )
 
+(define (generate-hatching
+         image
+         drawable
+         background
+         angle
+         mask-threshold)
+
+        ; initialize the hatch layers
+        ;   fill with plasma noise
+        ;   for some reason GEGL won't fill layer, so set active and trim
+        ;   ... then resize to match image again
+        ;   guassian blur to evan out the lines
+        ;   newsprint distort
+        (gegl-gegl
+            drawable
+            "plasma")
+        (gimp-image-set-active-layer
+            image
+            drawable)
+        (plug-in-autocrop-layer 
+            RUN-NONINTERACTIVE
+            image 
+            drawable)
+        (gimp-layer-scale
+            drawable
+            (car (gimp-drawable-width background))
+            (car (gimp-drawable-height background))
+            FALSE)
+        (gimp-drawable-desaturate
+            drawable
+            1)
+        (plug-in-gauss
+            RUN-NONINTERACTIVE
+            image ; unused 
+            drawable
+            15.5
+            15.5
+            0)
+        (plug-in-newsprint
+            RUN-NONINTERACTIVE
+            image ; unused 
+            drawable
+            6 ; cell-width
+            0 ; colorspace (0=GRAYSCALE)
+            0 ; kpullout CKMY only
+            angle ; angle
+            1 ; lines
+            0
+            0
+            0
+            0
+            0
+            0
+            3 ; oversampling
+        )
+        (plug-in-ripple
+            RUN-NONINTERACTIVE
+            image ; unused 
+            drawable
+            100 ; period
+            3 ; amplitude
+            0 ; orientation
+            1 ; edges WRAP
+            1 ; waveform SINE
+            TRUE ; antialias
+            TRUE)  ; tileable
+
+        ; generate and apply the mask
+        (let* (
+                (mask (car (gimp-layer-copy background TRUE)))
+            )
+
+            (gimp-image-insert-layer image mask 0 -1)
+            ; oilify 8
+            ; desaturate
+            ; posterize 7
+            ; threshold
+            ; black to alpha
+            ; guassian blur 5.5
+            (plug-in-oilify
+                RUN-NONINTERACTIVE
+                image ; unused
+                mask
+                8
+                1)
+            (gimp-drawable-desaturate
+                mask
+                1)
+            (gimp-drawable-posterize
+                mask
+                7)
+            (gimp-drawable-threshold
+                mask
+                0 ; channel VALUE
+                mask-threshold
+                1.0)
+            (plug-in-colortoalpha
+                RUN-NONINTERACTIVE
+                image ; unused 
+                mask
+                '(0 0 0))
+            (plug-in-gauss
+                RUN-NONINTERACTIVE
+                image ; unused 
+                mask
+                5.5
+                5.5
+                0)
+
+            (gimp-file-save 
+            RUN-NONINTERACTIVE 
+            image 
+            mask 
+            "buffer.png"
+            "buffer.png")
+
+            (gimp-image-raise-item-to-top image drawable)
+            (gimp-image-raise-item-to-top image mask)
+            (gimp-image-set-active-layer image (car (gimp-image-merge-down image mask 1)))
+
+            ; we want drawable to reference the top layer now
+            (set! drawable (car (gimp-image-get-active-layer image)))
+        )
+
+        ; finally remove the rest of the white
+        (plug-in-colortoalpha
+            RUN-NONINTERACTIVE
+            image ; unused 
+            drawable
+            '(255 255 255))
+
+        '(drawable)
+)
+
 (define (rotoscope 
         filename
         outputfile
@@ -24,19 +158,16 @@
             (background (car (gimp-image-get-active-layer image)))
             (foreground (car (gimp-layer-copy background TRUE)))
             (hatch1 (car (new-layer-same-size image background)))
-            (hatch1_mask (car (gimp-layer-copy background TRUE)))
             (hatch2 (car (new-layer-same-size image background)))
-            (hatch2_mask (car (gimp-layer-copy background TRUE)))
         )
 
         ; insert the layers into the image
         (gimp-image-insert-layer image foreground 0 -1)
         (gimp-image-insert-layer image hatch1 0 -1)
-        (gimp-image-insert-layer image hatch1_mask 0 -1)
         (gimp-image-insert-layer image hatch2 0 -1)
-        (gimp-image-insert-layer image hatch2_mask 0 -1)
 
         ; create the background color with a wide oilify plus waterpixel
+        ; then oilify again to smooth out the edges 
         (plug-in-oilify
             RUN-NONINTERACTIVE
             image ; unused
@@ -46,6 +177,12 @@
         (gegl-gegl
             background
             "waterpixels size=64")
+        (plug-in-oilify
+            RUN-NONINTERACTIVE
+            image ; unused
+            background
+            12
+            1)
 
         ; create the foreground:
         ;   oilify size 6 to simplify the image
@@ -88,46 +225,23 @@
         ;   ... then resize to match image again
         ;   newsprint distort @ 45 degrees
         ;   mirror to second hatch layer
-        (gegl-gegl
-            hatch1
-            "plasma")
-        (gimp-image-set-active-layer
+        (generate-hatching
             image
-            hatch1)
-        (plug-in-autocrop-layer 
-            RUN-NONINTERACTIVE
-            image 
-            hatch1)
-        (gimp-layer-scale
             hatch1
-            (car (gimp-drawable-width background))
-            (car (gimp-drawable-height background))
-            FALSE)
-        ;(gimp-drawable-curves-spline
-        ;    hatch1
-        ;    0
-        ;    4
-        ;    #(0 50 255 225))
-        (gimp-drawable-desaturate
-            hatch1
-            1)
-        (plug-in-newsprint
-            RUN-NONINTERACTIVE
-            image ; unused 
-            hatch1
-            6 ; cell-width
-            0 ; colorspace (0=GRAYSCALE)
-            0 ; kpullout CKMY only
-            55 ; angle
-            1 ; lines
-            0
-            0
-            0
-            0
-            0
-            0
-            1 ; oversampling
-        )
+            background
+            125
+            0.25)
+        (set! hatch1 (car (gimp-image-get-active-layer image)))
+    
+        (generate-hatching
+            image
+            hatch2
+            background
+            55
+            0.4)
+        (set! hatch2 (car (gimp-image-get-active-layer image)))
+
+        (gimp-image-raise-item-to-top image foreground)
 
         (gimp-file-save 
             RUN-NONINTERACTIVE 
@@ -162,6 +276,21 @@
                 (new_image (car (gimp-image-duplicate image)))
                 (drawable (car (gimp-image-merge-visible-layers new_image 1)))
             )
+
+            ;(gimp-image-insert-layer new_image (car (gimp-layer-copy background TRUE)) 0 -1)
+            ;(gimp-image-insert-layer new_image (car (gimp-layer-copy hatch1 TRUE)) 0 -1)
+            ;(gimp-image-insert-layer new_image (car (gimp-layer-copy hatch2 TRUE)) 0 -1)
+            ;(gimp-image-insert-layer new_image (car (gimp-layer-copy foreground TRUE)) 0 -1)
+
+           ; (gimp-edit-copy background)
+           ; (gimp-edit-paste drawable TRUE)
+           ; (gimp-edit-copy hatch1)
+           ; (gimp-edit-paste drawable TRUE)
+           ; (gimp-edit-copy hatch2)
+           ; (gimp-edit-paste drawable TRUE)
+           ; (gimp-edit-copy foreground)
+           ; (gimp-edit-paste drawable TRUE)
+
             (gimp-file-save 
                 RUN-NONINTERACTIVE 
                 new_image 
